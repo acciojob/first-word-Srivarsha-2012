5a9ccb779d90e9a8cd921247266e94c36bbf7735
srivarsha <algotesrivarsha@gmail.com>
2026-04-01 16:41:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+    // Remove leading spaces
+    str = str.trimStart();
+    
+    // Find the first space
+    let index = str.indexOf(' ');
+    
+    // If no space found, return the whole string
+    if (index === -1) {
+        return str;
+    }
+    
+    // Return substring from start to first space
+    return str.substring(0, index);
 }
-
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
